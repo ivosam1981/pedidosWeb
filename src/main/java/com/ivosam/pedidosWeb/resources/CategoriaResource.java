8bb5afd5fd3684dae508209003e39aeff46dd37b
ivosam <ivgest@gmail.com>
2021-07-22 20:31:42 -0300

    testando a claase de dominio categoria

--- src/main/java/com/ivosam/pedidosWeb/resources/CategoriaResource.java
+++ src/main/java/com/ivosam/pedidosWeb/resources/CategoriaResource.java
@@ -1,16 +1,25 @@
 package com.ivosam.pedidosWeb.resources;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ivosam.pedidosWeb.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "Rest está funcionando";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1l, "Informatica");
+		Categoria cat2 = new Categoria(2l, "Escritorio");
+		
+		return Arrays.asList(cat1, cat2);
 	}
 	
 	
