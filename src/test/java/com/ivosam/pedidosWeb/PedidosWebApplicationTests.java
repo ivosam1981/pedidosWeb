ebab0a6c72039f364c977bac0f0dbc4e6be00ce3
ivosam <ivgest@gmail.com>
2021-07-22 19:42:28 -0300

    projeto criado

---
+++ src/test/java/com/ivosam/pedidosWeb/PedidosWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivosam.pedidosWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedidosWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
